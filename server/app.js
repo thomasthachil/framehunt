b27c89048318d992fca21198276780e764bd9726
Thomas Thachil <thachil@gatech.edu>
2017-10-14 19:34:04 -0400

    added full server stuff

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var index = require('./index');
 
 var app = express();
 
