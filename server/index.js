3592616ab3687db5c510ea333d6426347e20a3cb
Tommaso Pieroncini <tommy.pieroncini@gatech.edu>
2017-10-14 19:38:26 -0400

    Added youtube method in servere

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+var youtubedl = require('youtube-dl');
+var FileReader = require('filereader');
 
 /* GET home page. */
 router.get('/bytes', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  var filename = req.query.file;
-  console.log(filename);
-  console.log("received");
-  res.send(fs.readFileSync(filename, { encoding: 'base64' }));
+    var video = youtubedl(req.query.url,
+    // Optional arguments passed to youtube-dl.
+    ['--format=18'],
+    // Additional options can be given for calling `child_process.execFile()`.
+    { cwd: __dirname });
+
+    var fileReader = new FileReader();
+    fileReader.readAsDataURL(video);
+    res.send(fileReader.result);
+
+  // // res.render('index', { title: 'Express' });
+  // var filename = req.query.file;
+  // console.log(filename);
+  // console.log("received");
+  // res.send(fs.readFileSync(filename, { encoding: 'base64' }));
 });
 
 module.exports = router;
