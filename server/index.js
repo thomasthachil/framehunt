23b815ca933aba227c72c3604185fd24c6cee1ec
Thomas Thachil <thachil@gatech.edu>
2017-10-14 22:12:28 -0400

    server file saving

--- server/index.js
+++ server/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 var youtubedl = require('youtube-dl');
 var FileReader = require('filereader');
+var multer  = require('multer');
+var fs = require('fs');
 
-// Get byte stream for video
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../inputs/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+var upload = multer({ 
+  storage: storage,
+  limits: {fileSize: 9 * 1000000}
+});
+
+// Get byte stream for youtube video
 router.get('/bytes', function(req, res, next) {
     console.log(req.query.url)
     var video = youtubedl(req.query.url,
@@ -17,6 +34,12 @@ router.get('/bytes', function(req, res, next) {
     res.send(fileReader.result);
 
   // res.send(fs.readFileSync(filename, { encoding: 'base64' }));
+});
+
+// Get byte stream for video
+router.post('/upload', upload.single('video'), function(req, res, next) {
+  var video = req.file;
+  res.send(fs.readFileSync(video.path, { encoding: 'base64' }));
 });
 
 module.exports = router;
