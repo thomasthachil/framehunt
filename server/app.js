3fbde17c66da66485477f11c4860101515746851
Thomas Thachil <thomasthachil@users.noreply.github.com>
2017-10-15 07:08:06 -0400

    Revert "Youtube"

--- server/app.js
+++ server/app.js
@@ -3,10 +3,6 @@ var express = require('express');
 var index = require('./index');
 var app = express();
 
-var cors = require('cors');
-
 app.use('/', index);
-
-app.use(cors());
 
 module.exports = app;
